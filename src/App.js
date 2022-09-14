eeb50bda1c3ed1944deeac04a0ccad2ec27ecefe
janhavi <janashinde394@gmail.com>
2022-09-15 00:16:50 +1000

    deleted junk code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import { useEffect, react, useState } from 'react';
-// import LoginForm from './LoginForm';
+import React from 'react';
 import Form from './Form';
 import NavBar from './NavBar';
 import Spheres from './Spheres';
@@ -8,48 +7,7 @@ import Goals from './Goals';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
-function App() {
-
-  const [apiData, setApidata] = useState(null);
-  const [formData, setFormData] = useState({
-    name: 'name',
-    email: 'email',
-    password: 'password'
-  });
-
-  // useEffect(
-  //   () => {
-  //     fetch('http://127.0.0.1:3000/')
-  //       .then(r => r.json())
-  //       .then(data => setApidata(data));
-  //   }, []
-  // )
-
-  function handleChange(event) {
-    
-    const name = event.target.name;
-    console.log(name)
-    let value = event.target.value;
-    const email = event.target.email;
-    
-    let emailval = event.target.value;
-    
-
-    
-
-    setFormData({
-      ...formData,
-      [name]: value,
-      [email]: emailval
-    });
-  }
-
- 
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log(formData);
-  }
-  
+function App() {  
 
   return (
     <Router>
@@ -71,10 +29,7 @@ function App() {
 
     </Switch>
 
-      <p>formData.name: {formData.name}</p>
-      {/*<p>formData.email:{formData.email}</p>
-      // <p>formData.password:{formData.password}</p> */}
-      {/* <LoginForm  handleChange={handleChange} formData={formData} handleSubmit={handleSubmit}/> */}
+      
       
       
    </Router>
